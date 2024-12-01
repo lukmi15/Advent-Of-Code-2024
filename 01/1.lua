455606e6f3b7f3978f7ea0f2f4859616da108708
lukmi15 <lukas.mirow@web.de>
2024-12-01 11:48:13 +0100

    Solved day 1

---
+++ 01/1.lua
@@ -0,0 +1,47 @@
+#!/usr/bin/env lua
+--[[Advent of Code 2024 day 1 part 1
+Author(s)		: Lukas Mirow
+Date of creation	: 2024-12-01
+]]
+
+
+--Reads lines from input file and parses both lists
+function read_input(fname)
+	local left_list = {}
+	local right_list = {}
+	for line in io.lines(fname)
+	do
+		for left_num, right_num in string.gmatch(line, "(%d+)%s+(%d+)")
+		do
+			table.insert(left_list, left_num)
+			table.insert(right_list, right_num)
+		end
+	end
+	return left_list, right_list
+end
+
+--Sorts lists and checks if theirs lengths match
+function prepare_lists(left_list, right_list)
+	table.sort(left_list)
+	table.sort(right_list)
+	if #left_list ~= #right_list
+	then
+		error("List lengths do not match up (left is has length `" .. tostring(#left_list) .. "` and right list has length `" .. tostring(#right_list) .. "`.")
+	end
+	return left_list, right_list
+end
+
+--Calculates total distances of sorted lists, according to the assignment
+function calc_distance(left_list, right_list)
+	local total_distance = 0
+	for i = 1, #left_list
+	do
+		total_distance = total_distance + math.abs(left_list[i] - right_list[i])
+		print("Distance of " .. tostring(left_list[i]) .. " and " .. tostring(right_list[i]) .. ": " .. tostring(math.abs(left_list[i] - right_list[i])) .. ", total: " .. tostring(total_distance))
+	end
+	return total_distance
+end
+
+local left_list, right_list = read_input("input.txt")
+left_list, right_list = prepare_lists(left_list, right_list)
+print(calc_distance(left_list, right_list))
